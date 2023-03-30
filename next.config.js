ce5faf3c7a3cca15914df6d7b21c07b5b9167fc2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-03-30 17:47:15 -0600

    update nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
