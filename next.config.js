4a2c5487a6eaab2102e39058e3f60ae2baac6cf8
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-03-30 17:53:11 -0600

    remove experimental app dir

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
