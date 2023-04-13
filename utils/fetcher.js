d15c6b6efefb7c3f49afc86fc2bdf0ec059c004c
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-04-13 11:18:56 -0600

    use production web catalog (#3)

---
+++ utils/fetcher.js
@@ -0,0 +1,16 @@
+export async function fetcher(url) {
+  const response = await fetch(url)
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!response.ok) {
+    const error = new Error(
+      `An error occurred while fetching data from URL: ${url}`,
+    )
+    // Attach extra info to the error object.
+    error.info = await response.json()
+    error.status = response.status
+    throw error
+  }
+  const data = await response.json()
+  return data
+}
