88a135812987f0efdb08d460caafc2b48792a3e5
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-02-20 17:35:42 -0800

    add default thumbnails

--- components/dataset-card.js
+++ components/dataset-card.js
@@ -9,7 +9,7 @@ import {
   Thumbnail,
 } from '@/components/dataset'
 
-export const DatasetCard = ({ dataset }) => {
+export const DatasetCard = ({ dataset, index }) => {
   const {
     name,
     providers,
@@ -32,37 +32,47 @@ export const DatasetCard = ({ dataset }) => {
         sx={{
           mb: [5, 7, 7, 8],
           width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          position: 'relative',
         }}
       >
-        <Tags tags={tags} demo={demo} />
-        <Thumbnail
-          thumbnail={thumbnail}
-          name={name}
-          demo={demo}
-          stores={stores}
-        />
-        <Box mt={3}>
-          <Text sx={{ fontSize: 3, fontWeight: 400 }}>{name}</Text>
+        <Box sx={{ height: '60px', mb: '20px' }}>
+          <Tags tags={tags} demo={demo} />
+        </Box>
+        <Box sx={{ flex: 1, position: 'relative', height: '200px' }}>
+          <Thumbnail
+            thumbnail={thumbnail}
+            demo={demo}
+            stores={stores}
+            index={index}
+          />
         </Box>
 
-        <Box mt={3}>
-          <Text
-            sx={{
-              fontSize: 1,
-              maxHeight: 100,
-              maxWidth: '100%',
-              overflow: 'auto',
-            }}
-          >
-            {description}
-          </Text>
+        <Box sx={{ marginTop: '220px' }}>
+          <Box mt={3}>
+            <Text sx={{ fontSize: 3, fontWeight: 400 }}>{name}</Text>
+          </Box>
+
+          <Box mt={3}>
+            <Text
+              sx={{
+                fontSize: 1,
+                maxHeight: 100,
+                maxWidth: '100%',
+                overflow: 'auto',
+              }}
+            >
+              {description}
+            </Text>
+          </Box>
+
+          <License license={license} />
+
+          {!demo && <Maintainers maintainers={maintainers} />}
+          {links && <Links links={links} doi_citation={doi_citation} />}
+          {stores.length > 0 && <Stores stores={stores} />}
         </Box>
-
-        <License license={license} />
-
-        {!demo && <Maintainers maintainers={maintainers} />}
-        {links && <Links links={links} doi_citation={doi_citation} />}
-        {stores.length > 0 && <Stores stores={stores} />}
       </Box>
     </>
   )
--- components/dataset/thumbnail.js
+++ components/dataset/thumbnail.js
@@ -1,34 +1,78 @@
-import { Box, Image, Link } from 'theme-ui'
 import { Badge } from '@carbonplan/components'
 import { alpha } from '@theme-ui/color'
+import { Box } from 'theme-ui'
 
-export const Thumbnail = ({ thumbnail, name, demo }) => {
-  const fallbackThumbnail = 'https://via.placeholder.com/400x200'
+export const Thumbnail = ({ thumbnail, demo, index = 0 }) => {
+  const fallbackThumbnails = [
+    'https://images.unsplash.com/photo-1563974318767-a4de855d7b43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80',
+    'https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80',
+    'https://images.unsplash.com/photo-1584267759777-8a74a4f72a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fG1ldGVvcm9sb2d5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1513553404607-988bf2703777?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+    'https://images.unsplash.com/photo-1583325958573-3c89e40551ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+    'https://images.unsplash.com/photo-1580193813605-a5c78b4ee01a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+  ]
+
+  const fallbackThumbnail =
+    fallbackThumbnails[index % fallbackThumbnails.length]
+
+  const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'teal',
+    'blue',
+    'cyan',
+    'purple',
+    'pink',
+  ]
+
+  const color = colors[index % colors.length]
+
+  const image = thumbnail ?? fallbackThumbnail
 
   return (
-    <Box mt={3} sx={{ position: 'relative' }}>
-      <Link>
-        <Image
-          src={thumbnail ?? fallbackThumbnail}
-          alt={`Thumbnail for ${name}`}
-          sx={{
-            width: '100%',
-          }}
-        />
-        {demo && (
+    <>
+      <Box>
+        <Box>
           <Box
             sx={{
+              backgroundImage: `url(${image})`,
               position: 'absolute',
-              top: 0,
+              height: '200px',
               width: '100%',
-              backgroundColor: alpha('secondary', 0.4),
-              height: 'calc(100% - 5px)',
+              filter: 'grayscale(100%)',
+              backgroundSize: 'cover',
             }}
-          >
-            <Badge sx={{ mt: 2, ml: 2 }}>Demo</Badge>
-          </Box>
-        )}
-      </Link>
-    </Box>
+          />
+          <Box
+            sx={{
+              backgroundColor: alpha(color, 0.4),
+              position: 'absolute',
+              height: '200px',
+              width: '100%',
+              opacity: 0.7,
+              mixBlendMode: 'dodge',
+            }}
+          />
+        </Box>
+
+        <Box>
+          {demo && (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                width: '100%',
+                backgroundColor: alpha('secondary', 0.4),
+                height: 'calc(100% - 5px)',
+              }}
+            >
+              <Badge sx={{ mt: 2, ml: 2 }}>Demo</Badge>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </>
   )
 }
