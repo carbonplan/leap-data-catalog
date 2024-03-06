547a3a1d76e9fe2696c0df97d037cf7669e80dee
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2024-03-06 12:49:17 -0800

    Refactor DatasetCard component to use unique hash for thumbnail and color selection (#26)

--- components/dataset-card.js
+++ components/dataset-card.js
@@ -1,4 +1,8 @@
 import { Box, Text } from 'theme-ui'
+import {
+  getUniqueHashFromString,
+  getRandomIndexFromHash,
+} from '@/utils/string-hash'
 
 import {
   License,
@@ -9,7 +13,7 @@ import {
   Thumbnail,
 } from '@/components/dataset'
 
-export const DatasetCard = ({ dataset, index }) => {
+export const DatasetCard = ({ dataset }) => {
   const {
     name,
     providers,
@@ -27,19 +31,15 @@ export const DatasetCard = ({ dataset, index }) => {
   } = dataset
 
   const fallbackThumbnails = [
+    'https://images.unsplash.com/photo-1583325958573-3c89e40551ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
+    'https://images.unsplash.com/photo-1580193813605-a5c78b4ee01a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
+    'https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?q=80&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1518627675569-e9d4fb90cdb5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1563974318767-a4de855d7b43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
     'https://images.unsplash.com/photo-1454789476662-53eb23ba5907?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
     'https://images.unsplash.com/photo-1584267759777-8a74a4f72a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fG1ldGVvcm9sb2d5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&q=60',
     'https://images.unsplash.com/photo-1513553404607-988bf2703777?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1583325958573-3c89e40551ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1580193813605-a5c78b4ee01a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?q=80&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    '',
   ]
-
-  const fallbackThumbnail =
-    fallbackThumbnails[index % fallbackThumbnails.length]
 
   const colors = [
     'midnightBlue',
@@ -60,7 +60,13 @@ export const DatasetCard = ({ dataset, index }) => {
     'cyan',
   ]
 
-  const color = colors[index % colors.length]
+  const hash = getUniqueHashFromString(name)
+  const thumbnailIndex = getRandomIndexFromHash(hash, fallbackThumbnails.length)
+  const colorIndex = getRandomIndexFromHash(hash, colors.length)
+
+  const fallbackThumbnail = fallbackThumbnails[thumbnailIndex]
+
+  const color = colors[colorIndex]
 
   return (
     <>
