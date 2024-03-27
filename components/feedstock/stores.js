b6a4354a8d9fa35fb6ca51f8f68d57220380971a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-03-27 14:59:53 -0700

    Add data-viewer integration and update Store component in stores.js

--- components/feedstock/stores.js
+++ components/feedstock/stores.js
@@ -1,5 +1,6 @@
 import { Box, Flex, Text } from 'theme-ui'
-import { Button, Expander } from '@carbonplan/components'
+import { Button, Expander, Link } from '@carbonplan/components'
+import { RotatingArrow } from '@carbonplan/icons'
 import { Code } from '@carbonplan/prism'
 import AnimateHeight from 'react-animate-height'
 
@@ -74,7 +75,37 @@ const Store = ({ name, url }) => {
           >
             {copied ? 'Copied' : 'Copy'}
           </Button>
+
           <Code language='python'>{getSnippet(url)}</Code>
+
+          <Button
+            size={'xs'}
+            href={`https://data-viewer-git-katamartin-serverless-carbonplan.vercel.app/?dataset=${url}`}
+            suffix={
+              <RotatingArrow
+                sx={{ ml: '2px', mt: '-1px', width: 14, height: 14 }}
+              />
+            }
+            sx={{
+              mt: 3,
+              mb: 2,
+              p: 2,
+              bg: 'primary',
+              color: 'background',
+              fontFamily: 'mono',
+              textAlign: 'center',
+              fontSize: 1,
+              fontWeight: 'bold',
+              cursor: 'pointer',
+              '&:hover': {
+                bg: 'secondary',
+              },
+              borderRadius: 'small',
+              alignSelf: 'center',
+            }}
+          >
+            Open in Data Viewer
+          </Button>
         </Box>
       </AnimateHeight>
     </Box>
