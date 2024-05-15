7ccabc53c374b335edca28d5c2e270bc45e181c4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-05-15 15:20:59 -0700

    add Repository component for displaying repository information

---
+++ components/feedstock/repo.js
@@ -0,0 +1,49 @@
+import { Link } from '@carbonplan/components'
+import { Flex, Text } from 'theme-ui'
+
+function getOrgAndRepo(url) {
+  const urlParts = url.split('/')
+  const org = urlParts[3]
+  const repo = urlParts[4]
+  return `${org}/${repo}`
+}
+export const Repository = ({ repo }) => {
+  const orgAndRepo = getOrgAndRepo(repo)
+  const githubUrl = `https://github.com/${orgAndRepo}`
+
+  return (
+    <Flex
+      sx={{
+        flexDirection: 'column',
+        gap: 2,
+        justifyContent: 'space-between',
+        mt: 4,
+      }}
+    >
+      <Text
+        sx={{
+          color: 'muted',
+          fontSize: 0,
+          fontFamily: 'mono',
+          letterSpacing: 'mono',
+          textTransform: 'uppercase',
+        }}
+      >
+        Repository
+      </Text>
+      <Flex sx={{ flexDirection: 'row', gap: 2 }}>
+        <Link
+          href={githubUrl}
+          target='_blank'
+          rel='noopener noreferrer'
+          sx={{
+            textDecoration: 'none',
+            fontSize: [2, 2, 2, 3],
+          }}
+        >
+          {orgAndRepo}
+        </Link>
+      </Flex>
+    </Flex>
+  )
+}
