8ba267424d660e4527eba5c8df4f346a1a488334
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-03 14:53:55 -0700

    drop mouse listeners and boxshadow and transform styles

--- components/feedstock-card.tsx
+++ components/feedstock-card.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { License, Thumbnail } from '@/components/feedstock'
+import { Thumbnail } from '@/components/feedstock'
 import { Feedstock } from '@/types/types'
 import { alpha } from '@theme-ui/color'
-import { useState } from 'react'
-import { Box, Flex, Text } from 'theme-ui'
 import Link from 'next/link'
+import { Box, Flex, Text } from 'theme-ui'
 
 interface FeedstockCardProps {
   feedstock: Feedstock
@@ -14,10 +13,6 @@ interface FeedstockCardProps {
 export const FeedstockCard: React.FC<FeedstockCardProps> = ({ feedstock }) => {
   const { slug, description, provenance, tags, thumbnail, color, title } =
     feedstock
-
-  const { license, license_link } = provenance
-
-  const [isHovered, setIsHovered] = useState(false)
 
   const truncateDescription = (text: string, maxLength: number) => {
     if (text.length <= maxLength) {
@@ -38,8 +33,6 @@ export const FeedstockCard: React.FC<FeedstockCardProps> = ({ feedstock }) => {
           display: 'block',
           height: '100%', // Ensure full height
         }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
       >
         <Box
           id={slug}
@@ -49,11 +42,11 @@ export const FeedstockCard: React.FC<FeedstockCardProps> = ({ feedstock }) => {
             flexDirection: 'column',
             position: 'relative',
             transition: 'all 0.3s ease',
-            backgroundColor: isHovered ? alpha(color, 0.05) : 'transparent',
             padding: 3,
             borderRadius: 'default',
-            boxShadow: isHovered ? '0 4px 6px rgba(0, 0, 0, 0.1)' : 'none',
-            transform: isHovered ? 'translateY(-4px)' : 'none',
+            ':hover': {
+              backgroundColor: alpha(color, 0.05),
+            },
           }}
         >
           <Box sx={{ flex: '0 0 auto', position: 'relative' }}>
