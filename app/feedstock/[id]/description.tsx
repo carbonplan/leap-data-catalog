4c93cf3beb1033b896917ab7868906d60351a27b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-04 14:44:27 -0700

    refactor FeedstockDescription layout for improved spacing and structure

--- app/feedstock/[id]/description.tsx
+++ app/feedstock/[id]/description.tsx
@@ -2,7 +2,7 @@ import { Links } from '@/components/feedstock'
 import { Tags } from '@/components/feedstock/tags'
 import { Feedstock } from '@/types/types'
 import { Column, Row } from '@carbonplan/components'
-import { Box, Flex } from 'theme-ui'
+import { Box, Flex, Grid } from 'theme-ui'
 
 export const FeedstockDescription: React.FC<{ feedstock: Feedstock }> = ({
   feedstock,
@@ -12,14 +12,24 @@ export const FeedstockDescription: React.FC<{ feedstock: Feedstock }> = ({
       <Row columns={[1, 1, 12, 12]}>
         <Column start={[1, 1, 2, 2]} width={[1, 1, 6, 6]}>
           <Flex sx={{ flexDirection: 'column', gap: [3, 4] }}>
-            {feedstock.tags && <Tags tags={feedstock.tags} />}
-            <Box sx={{ fontSize: [2, 2, 2, 3], mb: 2, py: [1] }}>
-              {feedstock.description}
-            </Box>
+            {feedstock.tags && (
+              <Box sx={{ mb: 2 }}>
+                <Tags tags={feedstock.tags} />
+              </Box>
+            )}
+            <Box sx={{ fontSize: [2, 2, 2, 3] }}>{feedstock.description}</Box>
           </Flex>
         </Column>
         <Column start={[1, 1, 9, 9]} width={[1, 1, 3, 3]}>
-          {feedstock.links && <Links links={feedstock.links} />}
+          <Box
+            sx={{
+              mt: feedstock.tags ? 'calc(46px + 0.5em)' : 0,
+              ml: [0, 0, -4, -5],
+              pl: [0, 0, 1, 0],
+            }}
+          >
+            {feedstock.links && <Links links={feedstock.links} />}
+          </Box>
         </Column>
       </Row>
     </Box>
