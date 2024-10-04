3107324a9df930f2aedfc6fb79d28ede95f177e5
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-04 13:07:47 -0700

    update alignment of details' items (license, providers, etc)

--- app/feedstock/[id]/details.tsx
+++ app/feedstock/[id]/details.tsx
@@ -6,31 +6,32 @@ import {
 } from '@/components/feedstock'
 import { Feedstock } from '@/types/types'
 import { Column, Row } from '@carbonplan/components'
-import { Box, Flex } from 'theme-ui'
+import { Box, Grid } from 'theme-ui'
 
 export const FeedstockDetails: React.FC<{ feedstock: Feedstock }> = ({
   feedstock,
 }) => {
   return (
     <Box sx={{ ml: [3, 4, 5, 6] }}>
-      <Row columns={[1, 1, 12, 12]}>
-        <Column start={[1, 1, 2, 2]} width={[1, 1, 10, 10]}>
-          <Flex
-            sx={{
-              flexDirection: ['column', 'column', 'row', 'row'],
-              gap: [4, 4, 6, 6],
-              justifyContent: 'space-between',
-              alignItems: ['stretch', 'flex-start'],
-            }}
-          >
-            <Maintainers maintainers={feedstock.maintainers} />
-            <License
-              license={feedstock.provenance?.license}
-              license_link={feedstock.provenance?.license_link}
-            />
-            <Repository metaURL={feedstock['ncviewjs:meta_yaml_url']} />
-            <Providers providers={feedstock.provenance?.providers} />
-          </Flex>
+      <Row>
+        <Column start={[1, 1, 2, 2]} width={[6, 6, 8, 10]}>
+          <Grid columns={[1, 1, 2, 2]}>
+            <Box>
+              <Maintainers maintainers={feedstock.maintainers} />
+            </Box>
+            <Box>
+              <License
+                license={feedstock.provenance?.license}
+                license_link={feedstock.provenance?.license_link}
+              />
+            </Box>
+            <Box>
+              <Repository metaURL={feedstock['ncviewjs:meta_yaml_url']} />
+            </Box>
+            <Box>
+              <Providers providers={feedstock.provenance?.providers} />
+            </Box>
+          </Grid>
         </Column>
       </Row>
     </Box>
