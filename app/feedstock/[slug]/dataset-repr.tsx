013e7068ad276b7955c01c4ef26f3583e23e3d2c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-08 17:34:02 -0700

    refactor: replace SWR with getDatasetRepr utility for dataset representation

--- app/feedstock/[slug]/dataset-repr.tsx
+++ app/feedstock/[slug]/dataset-repr.tsx
@@ -1,7 +1,6 @@
-import { fetcher } from '@/utils/fetcher'
+import { getDatasetRepr } from '@/utils/get-xarray-html-repr'
 import { Info } from '@carbonplan/icons'
 import React from 'react'
-import useSWR from 'swr'
 import { Alert, Box, Card, Flex, Text } from 'theme-ui'
 
 interface DatasetReprProps {
@@ -15,62 +14,30 @@ interface DatasetRepr {
 }
 
 export const DatasetRepr: React.FC<DatasetReprProps> = ({ url }) => {
-  const { data, error, isLoading } = useSWR<DatasetRepr>(
-    `/api/dataset-repr?url=${url}`,
-    fetcher,
-    {
-      suspense: true,
-      fallbackData: { html: '', url: '', sanitized_url: '' },
-      dedupingInterval: 3600,
-    },
-  )
-
-  if (error) {
+  try {
+    const data = getDatasetRepr(url)
+    return (
+      <Card sx={{ p: 3, borderRadius: 'default', width: '100%' }}>
+        <Box
+          dangerouslySetInnerHTML={{ __html: data.html }}
+          sx={{
+            overflowY: 'auto',
+            borderColor: 'muted',
+            borderRadius: 'default',
+            p: 2,
+            width: '100%',
+          }}
+        />
+      </Card>
+    )
+  } catch (error) {
     return (
       <Alert variant='primary' sx={{ p: 3, borderRadius: 'default' }}>
         <Flex sx={{ flexDirection: 'row', gap: 4 }}>
           <Info />
-
           <Text>Failed to load dataset representation: {error.message}</Text>
         </Flex>
       </Alert>
     )
   }
-
-  if (isLoading) {
-    return (
-      <>
-        <Card sx={{ p: 3, borderRadius: 'default', width: '100%' }}>
-          <Box
-            sx={{
-              height: '200px',
-              bg: 'muted',
-              width: '100%',
-              borderColor: 'muted',
-              borderRadius: 'default',
-            }}
-          />
-        </Card>
-      </>
-    )
-  }
-
-  return (
-    <>
-      <Card sx={{ p: 3, borderRadius: 'default', width: '100%' }}>
-        {data?.html ? (
-          <Box
-            dangerouslySetInnerHTML={{ __html: data?.html }}
-            sx={{
-              overflowY: 'auto',
-              borderColor: 'muted',
-              borderRadius: 'default',
-              p: 2,
-              width: '100%',
-            }}
-          />
-        ) : null}
-      </Card>
-    </>
-  )
 }
