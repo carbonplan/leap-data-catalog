9ab118309defd54ac97b9a18fad712816266f82e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-15 03:39:10 -0700

    fix: adjust column widths and start positions for improved layout in FeedstockDetails component

--- app/feedstock/[slug]/details.tsx
+++ app/feedstock/[slug]/details.tsx
@@ -14,19 +14,19 @@ export const FeedstockDetails: React.FC<{ feedstock: Feedstock }> = ({
   return (
     <>
       <Row>
-        <Column start={[1, 1, 2, 2]} width={[6, 4, 3, 3]}>
+        <Column start={[1, 1, 2, 2]} width={[6, 4, 3, 5]}>
           <Maintainers maintainers={feedstock.maintainers} />
         </Column>
-        <Column start={[1, 5, 4, 4]} width={[6, 4, 3, 3]}>
+        <Column start={[1, 5, 7, 7]} width={[6, 4, 3, 3]}>
           <License
             license={feedstock.provenance?.license}
             license_link={feedstock.provenance?.license_link}
           />
         </Column>
-        <Column start={[1, 1, 7, 7]} width={[6, 4, 3, 3]}>
+        <Column start={[1, 1, 2, 2]} width={[6, 4, 3, 3]}>
           <Repository metaURL={feedstock['ncviewjs:meta_yaml_url']} />
         </Column>
-        <Column start={[1, 5, 10, 10]} width={[6, 4, 3, 3]}>
+        <Column start={[1, 5, 7, 7]} width={[6, 4, 3, 3]}>
           <Providers providers={feedstock.provenance?.providers} />
         </Column>
       </Row>
