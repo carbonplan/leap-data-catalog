9ea181a1e8d65fd94ba6fc1b690cd4de6d2e1501
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2024-12-16 16:20:07 -0800

    Add preserveQuery prop to BackButton for query parameter handling (#97)

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,27 +1,37 @@
 'use client'
 import { Button } from '@carbonplan/components'
 import { Left } from '@carbonplan/icons'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { ThemeUIStyleObject } from 'theme-ui'
 
 interface BackButtonProps {
   href?: string
+  preserveQuery?: boolean
   sx?: ThemeUIStyleObject
 }
 
-export const BackButton: React.FC<BackButtonProps> = ({ href = '/', sx }) => {
+export const BackButton: React.FC<BackButtonProps> = ({
+  href = '/',
+  preserveQuery = false,
+  sx,
+}) => {
   const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const handleClick = () => {
+    if (preserveQuery) {
+      const currentParams = new URLSearchParams(searchParams.toString())
+      const newUrl = `${href}${
+        currentParams.toString() ? '?' + currentParams.toString() : ''
+      }`
+      router.push(newUrl)
+    } else {
+      router.push(href)
+    }
+  }
 
   return (
-    <Button
-      inverted
-      size='xs'
-      onClick={() => {
-        router.push(href)
-      }}
-      prefix={<Left />}
-      sx={sx}
-    >
+    <Button inverted size='xs' onClick={handleClick} prefix={<Left />} sx={sx}>
       Back
     </Button>
   )
