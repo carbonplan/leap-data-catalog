74e371f65e10725eb1ef21f25cefb0e0ef8cb4cc
Kata Martin <katasm@gmail.com>
2025-04-28 16:08:38 -0400

    Implement functionality using react-select

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -5,15 +5,18 @@ import { SearchBox } from '@/components/search-box'
 import { Feedstock } from '@/types/types'
 import { Column, Row } from '@carbonplan/components'
 import { Suspense, useMemo, useState } from 'react'
+import Select, { MultiValue } from 'react-select'
 import { Box, Text, Container } from 'theme-ui'
 
 const FeedstockList = ({
   feedstocks,
   search,
+  tags,
   catalog,
 }: {
   feedstocks: Feedstock[]
   search: string
+  tags: string[]
   catalog?: string
 }) => {
   const filteredFeedstocks = useMemo(() => {
@@ -21,19 +24,27 @@ const FeedstockList = ({
       return []
     }
 
-    if (!search) {
-      return feedstocks
+    let result = feedstocks
+
+    if (search) {
+      const re = new RegExp(search, 'i')
+
+      result = result.filter(
+        (d: Feedstock) =>
+          d.title.match(re) ||
+          d.tags?.some((tag) => tag.match(re)) ||
+          d.description.match(re),
+      )
     }
 
-    const re = new RegExp(search, 'i')
+    if (tags.length > 0) {
+      result = result.filter((d: Feedstock) =>
+        tags.find((tag) => d.tags?.includes(tag)),
+      )
+    }
 
-    return feedstocks.filter(
-      (d: Feedstock) =>
-        d.title.match(re) ||
-        d.tags?.some((tag) => tag.match(re)) ||
-        d.description.match(re),
-    )
-  }, [feedstocks, search])
+    return result
+  }, [feedstocks, search, tags])
 
   if (!feedstocks || feedstocks.length === 0) {
     return <Box />
@@ -62,8 +73,30 @@ type CatalogProps = {
   catalog?: string
 }
 
+type TagOption = {
+  value: string
+  label: string
+}
+
 export const Catalog = ({ feedstocks, error, catalog }: CatalogProps) => {
   const [search, setSearch] = useState('')
+  const [selectedTags, setSelectedTags] = useState<MultiValue<TagOption>>([])
+
+  const tags = useMemo(() => {
+    const allTags = feedstocks.reduce((accum, f) => {
+      f.tags?.forEach((tag) => accum.add(tag))
+      return accum
+    }, new Set<string>())
+
+    return Array.from(allTags).map((tag) => ({
+      value: tag,
+      label: tag.toUpperCase(),
+    }))
+  }, [feedstocks])
+
+  const selectedTagValues = useMemo(() => {
+    return selectedTags.map((d) => d.value)
+  }, [selectedTags])
 
   if (error) {
     return (
@@ -91,12 +124,19 @@ export const Catalog = ({ feedstocks, error, catalog }: CatalogProps) => {
             </Column>
             <Column start={[1, 5, 5, 5]} width={[6, 4, 6, 6]}>
               <SearchBox search={search} setSearch={setSearch} />
+              <Select
+                value={selectedTags}
+                onChange={setSelectedTags}
+                options={tags}
+                isMulti
+              />
             </Column>
           </Row>
 
           <Box mt={3}>
             <Suspense fallback={<Box />}>
               <FeedstockList
+                tags={selectedTagValues}
                 feedstocks={feedstocks}
                 search={search}
                 catalog={catalog}
